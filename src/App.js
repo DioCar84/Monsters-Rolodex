d5cfb303d31206e032b99efc0f23b49ecf6f2073
Diogo Carvalho <dcar84@gmail.com>
2022-08-30 14:31:18 +0000

    Add functional components

--- src/App.js
+++ src/App.js
@@ -1,110 +1,45 @@
-// import logo from "./logo.svg";
-import "./App.css";
-import { Component } from "react";
+import { useState, useEffect } from 'react';
 
-class App extends Component {
-  constructor() {
-    super();
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+import './App.css';
 
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
-  }
+      .then((users) => setMonsters(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchField };
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
 
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchField);
-    });
-
-    return (
-      <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={onSearchChange}
-        />
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+      <SearchBox
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange}
+        placeholder='search monsters'
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
-
-// class App extends Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       name: { firstName: "Diogo", lastName: "Carvalho" },
-//       company: "Mondrian Investment Partners",
-//     };
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
-//             {this.state.company}.
-//           </p>
-//           <button
-//             onClick={() => {
-//               // /* changes state to a diff object(shallow merge), react only rerenders when this happens */
-//               // /* has to match the state object, ex: so if a string vs object it won't work */
-//               // this.setState({ name: { firstName: "Jimmy", lastName: "Jam" } });
-
-//               /* this makes setState act more 'synchronis', runs first functions then runs the second callback only after first finishes */
-//               this.setState(
-//                 () => {
-//                   return {
-//                     name: { firstName: "Jimmy", lastName: "Jam" },
-//                   };
-//                 },
-//                 () => {
-//                   console.log(this.state);
-//                 }
-//               );
-//             }}
-//           >
-//             Change Name
-//           </button>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
